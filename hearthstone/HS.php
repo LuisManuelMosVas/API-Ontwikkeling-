2539c315e349bf5cadc054e98e11a664c00e7e07
LuisManuelMosVas <53187844+LuisManuelMosVas@users.noreply.github.com>
2020-05-29 14:46:38 +0200

    V1.0
    API WERKT
    ALLE CARDS WERKT
    SEARCH IS MOELIJK

---
+++ hearthstone/HS.php
@@ -0,0 +1,12 @@
+<?php
+
+$curl = curl_init();
+
+$url= " ";
+echo'
+$url= <form class="bootstrap-class" action= "HSget.php" method="POST">
+  <input type="text" class="bootstrap-class" name=$url>
+  <input type="submit" class="bootstrap-class" name="sb" value="zoek lol" />
+</form>
+';
+
